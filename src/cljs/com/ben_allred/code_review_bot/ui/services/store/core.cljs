9bdde78d192aeac0dce0090df7ddf35c3484121c
Ben Allred <skuttleman@gmail.com>
2018-02-17 13:42:57 -0700

    Use log/spy to colorize store updates

--- src/cljs/com/ben_allred/code_review_bot/ui/services/store/core.cljs
+++ src/cljs/com/ben_allred/code_review_bot/ui/services/store/core.cljs
@@ -3,12 +3,13 @@
               [com.ben-allred.collaj.enhancers :as collaj.enhancers]
               [com.ben-allred.code-review-bot.ui.services.store.reducers :as reducers]
               [reagent.core :as r]
-              [com.ben-allred.code-review-bot.utils.maps :as maps :include-macros true]))
+              [com.ben-allred.code-review-bot.utils.maps :as maps]
+              [com.ben-allred.code-review-bot.utils.logging :as log]))
 
 (def ^:private store (collaj/create-custom-store r/atom
                          reducers/root
                          collaj.enhancers/with-fn-dispatch
-                         (collaj.enhancers/with-log-middleware (comp js/console.log pr-str))))
+                         (collaj.enhancers/with-log-middleware identity (fn [new-state] (log/spy new-state)))))
 
 (def get-state (:get-state store))
 
