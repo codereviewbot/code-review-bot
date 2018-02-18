603767552920183b0b492edfe3e0aca6423e39bd
Ben Allred <skuttleman@gmail.com>
2018-02-17 17:34:11 -0700

    script to seed local db with heroku data

---
+++ scripts/sync-db.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+regex='^{?"([^"]+)"[^"]+"([^"]+)"'
+
+echo 'loading env from ".lein-env"...'
+cat .lein-env | (while read line
+do
+    set -e
+    [[ "${line}" =~ $regex ]]
+    var="${BASH_REMATCH[1]}"
+    val="${BASH_REMATCH[2]}"
+    eval "export ${var}=${val}"
+done
+
+echo 'running mongo script...'
+echo "
+var remote = connect('${HEROKU_MONGODB_URI}');
+var local = connect('${MONGODB_URI}');
+
+['configs', 'users'].forEach(function(coll) {
+    local[coll].remove({});
+    local[coll].insertMany(remote[coll].find({}).toArray());
+});
+" | mongo > /dev/null)
+
+echo 'finished'
