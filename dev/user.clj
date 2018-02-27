58770f87a4f93b92c17e1c066ebcc2059eb73239
Ben Allred <skuttleman@gmail.com>
2018-02-27 04:56:58 -0700

    Use :remote-mongodb-uri in user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -11,9 +11,9 @@
 (defn update-env [key val]
     (alter-var-root #'com.ben-allred.code-review-bot.services.env/get assoc key val))
 
-(defn connect-to-heroku-db []
+(defn connect-to-remote-db []
     (alter-var-root #'com.ben-allred.code-review-bot.api.services.mongo/db
-        (let [conn-str (str (:heroku-mongodb-uri env/get) "?maxPoolSize=128&waitQueueMultiple=5;waitQueueTimeoutMS=150;socketTimeoutMS=5500;safe=false&w=1;wtimeoutms=5000;fsync=true")]
+        (let [conn-str (str (:remote-mongodb-uri env/get) "?maxPoolSize=128&waitQueueMultiple=5;waitQueueTimeoutMS=150;socketTimeoutMS=5500;safe=false&w=1;wtimeoutms=5000;fsync=true")]
             (constantly (delay (:db (mg/connect-via-uri conn-str)))))))
 
 (defn connect-to-local-db []
--- src/cljs/com/ben_allred/code_review_bot/ui/services/store/core.cljs
+++ src/cljs/com/ben_allred/code_review_bot/ui/services/store/core.cljs
@@ -4,12 +4,12 @@
               [com.ben-allred.code-review-bot.ui.services.store.reducers :as reducers]
               [reagent.core :as r]
               [com.ben-allred.code-review-bot.utils.maps :as maps]
-              [com.ben-allred.code-review-bot.utils.logging :as log]))
+              [com.ben-allred.code-review-bot.utils.logging :as log :include-macros true]))
 
 (defonce ^:private store (collaj/create-custom-store r/atom
                          reducers/root
                          collaj.enhancers/with-fn-dispatch
-                         (collaj.enhancers/with-log-middleware identity #_(fn [new-state] (log/spy new-state)))))
+                         (collaj.enhancers/with-log-middleware #(log/spy %) identity)))
 
 (def get-state (:get-state store))
 
